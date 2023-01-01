f62051f9e2dae9fb0f9429ebfd2068ed02a3dcb1
baba-s <shotababa1206@gmail.com>
2023-01-01 10:22:25 +0900

    add

--- Runtime/UniTaskAttachExternalCancellationExtensionMethods.cs
+++ Runtime/UniTaskAttachExternalCancellationExtensionMethods.cs
@@ -5,6 +5,15 @@ namespace Kogane
 {
     public static class UniTaskAttachExternalCancellationExtensionMethods
     {
+        public static UniTask AttachExternalCancellation
+        (
+            this UniTask self,
+            GameObject   gameObject
+        )
+        {
+            return self.AttachExternalCancellation( gameObject.GetCancellationTokenOnDestroy() );
+        }
+
         public static UniTask<T> AttachExternalCancellation<T>
         (
             this UniTask<T> self,
@@ -12,6 +21,15 @@ namespace Kogane
         )
         {
             return self.AttachExternalCancellation( gameObject.GetCancellationTokenOnDestroy() );
+        }
+
+        public static UniTask AttachExternalCancellation<TComponent>
+        (
+            this UniTask self,
+            TComponent   component
+        ) where TComponent : Component
+        {
+            return self.AttachExternalCancellation( component.GetCancellationTokenOnDestroy() );
         }
 
         public static UniTask<T> AttachExternalCancellation<T, TComponent>
